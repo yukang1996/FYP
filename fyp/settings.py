9ccb8103d2b0da06c82f55262a5cc35d8fcd5618
yukang1996 <yukang_1996@hotmail.com>
2018-12-05 10:50:07 +0800

    edited

--- fyp/settings.py
+++ fyp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y=lwzrq-ylb$8^1415-5*24dww=*zpie_#ps%ax8^*k(g-@8!x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['pothole-system.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
