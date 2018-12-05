4520b14ec8eb3a283a690327860e8acff7782130
yukang1996 <yukang_1996@hotmail.com>
2018-12-05 12:18:29 +0800

    edited

--- fyp/settings.py
+++ fyp/settings.py
@@ -76,12 +76,20 @@ WSGI_APPLICATION = 'fyp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+in_heroku = False
+if 'DATABASE_URL' in os.environ:
+    in_heroku = True
+
+import dj_database_url
+if in_heroku:
+    DATABASES = {'default': dj_database_url.config()}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
@@ -123,7 +131,3 @@ STATIC_ROOT = "/fyp/web/static"
 STATIC_URL = "/static/"
 MEDIA_ROOT = "/Users/User/Desktop/django tutorial/fyp/web/media/"
 MEDIA_URL = "/media/"
-
-if 'DATABASE_URL' in os.environ:
-    import dj_database_url
-    DATABASES = {'default': dj_database_url.config()}
